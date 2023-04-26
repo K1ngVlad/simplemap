6ac40f53eded5369584ec28569a2135d72f482c2
K1ngVlad <kingvlad3008@gmail.com>
2023-04-27 00:51:10 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { MapComponent, SideBar, WaterMark } from './components';
 import { MapProvider } from './contexts';
 
